ddde30093c405fedf1306d834bf445a97e2f66d4
Jack Hsiao <chhsiao@gms.tcu.edu.tw>
2025-09-25 20:06:07 +0800

    Add files via upload

---
+++ setting.js
@@ -0,0 +1,5 @@
+var FHIRrootURL ="https://hapi.fhir.org/baseR4/";
+
+//  var FHIRrootURL = "https://fhir.tcumi.com:58443/r5/fhir/"
+//var FHIRrootURL = "https://fhir.dicom.tw/fhir/";
+//https://fhir.dicom.tw/fhir/
